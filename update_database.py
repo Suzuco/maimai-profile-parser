cfb0f06daa4fcbc672397efc4f6053e083af56d3
BuildTools <unconfigured@null.spigotmc.org>
2022-04-23 21:57:26 +0800

    some more abbreviations

--- update_database.py
+++ update_database.py
@@ -35,10 +35,17 @@ abbrs = [('Sqlupp(Camellia)', """Sqlupp (Camellia's "Sqleipd*Hiytex" Remix)"""),
          ('Caliburne', """Caliburne ～Story of the Legendary sword～"""),
          ('otorii INNOVATED', """otorii INNOVATED -[i]3-"""),
          ('felys', """felys -final remix-"""),
+         ('AMAZING MIGHTY', """AMAZING MIGHTYYYY!!!!"""),
          ('ワンダーシャッフェン', """ワンダーシャッフェンの法則"""),
          ('キミツアー', """≠彡"/了→"""),
          ('シンフォニエッタ', """渦状銀河のシンフォニエッタ"""),
-         ('エンドマーク', """エンドマークに希望と涙を添えて""")]
+         ('エンドマーク', """エンドマークに希望と涙を添えて"""),
+         ('UNiVERSE 銀河鸞翔', """U&iVERSE -銀河鸞翔-"""),
+         ('壊れタ人形', """ねぇ、壊れタ人形ハ何処へ棄テらレるノ？"""),
+         ('バーチャルダムネーション', """バーチャルダム　ネーション"""),
+         ('雷切', """雷切-RAIKIRI-"""),
+         ('待チ人ハ来ズ', """待チ人ハ来ズ。"""),
+         ('チルノ9周年', """チルノのパーフェクトさんすう教室　⑨周年バージョン""")]
 
 for abbr, full in abbrs:
     _py = _py.replace(abbr, full)
